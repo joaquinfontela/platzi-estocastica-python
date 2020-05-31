a31ba1fa74dc569d52c724f45577a625c256b09f
joaquinfontela <joaquinfontela@gmail.com>
2020-05-30 21:32:39 -0300

    simulacion y grafica de caminos aleatorios

--- camino-borrachos/Borracho.py
+++ camino-borrachos/Borracho.py
@@ -1,3 +1,5 @@
+
+
 class Borracho:
     
     def __init__(self, nombre):
--- camino-borrachos/caminoAleatorio.py
+++ camino-borrachos/caminoAleatorio.py
@@ -1,6 +1,7 @@
 from BorrachoTradicional import BorrachoTradicional
 from Campo import Campo
 from Coordenada import Coordenada
+from bokeh.plotting import figure, show
 
 
 def caminata(campo, borracho, pasos):
@@ -29,20 +30,33 @@ def simularCaminatas(pasos, numeroDeIntentos, tipoDeBorracho):
     return distancias
 
 
+def graficar(x, y):
+    grafica = figure(title = 'Camino de Borrachos', x_axis_label='pasos', y_axis_label='distancia recorrida')
+    #Creamos una grafica a la cual le damos un titulo, y un nombre a las x e y.
+    grafica.line(x, y, legend='distancia media')
+    
+    show(grafica)
+
+
 def main(distanciasDeCaminata, numeroDeIntentos, tipoDeBorracho):
     
+    distanciaMediaPorCaminata = []
+    # Un array con la distancia media al origen por cada caminata.
     for pasos in distanciasDeCaminata:
         distancias = simularCaminatas(pasos, numeroDeIntentos, tipoDeBorracho)
         distanciaMedia = round( sum(distancias) / len(distancias), 4 )
+        distanciaMediaPorCaminata.append(distanciaMedia)
         distanciaMaxima = max(distancias)
         distanciaMinima = min(distancias)
         print(f'{tipoDeBorracho.__name__} camino {pasos} pasos, con una distancia media de {distanciaMedia}')
         print(f'La distancia maxima fue de {distanciaMaxima} y la minima de {distanciaMinima}.\n')
+        
+    graficar(distanciasDeCaminata, distanciaMediaPorCaminata)
 
 
 if __name__ == "__main__":
     
-    distanciasDeCaminata = [ 10, 100, 1000, 10000 ]
+    distanciasDeCaminata = range(0,5001,100)
     numeroDeIntentos = 100
     
     main(distanciasDeCaminata, numeroDeIntentos, BorrachoTradicional)
